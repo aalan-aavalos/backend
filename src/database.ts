f9af487acf548df9e1be23c7fa63ba6c0b3e8416
aalan <avalosalan789@gmail.com>
2023-08-17 17:21:47 -0600

    Se agrego

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { createPool } from 'mysql2/promise';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config';
+
+
+export const pool =createPool(
+    {
+        host:DB_HOST,
+        user:DB_USER,
+        password:DB_PASSWORD,
+        port:DB_PORT,
+        database:DB_NAME
+    }
+)
